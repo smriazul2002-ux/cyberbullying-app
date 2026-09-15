878dc3d8a10300bf3e7bea11d0aeccdd13386dd4
Shikdar Mohammad Riazul <smriazul2002@gmail.com>
2026-09-16 04:09:53 +0800

    disable stale Flutter web service worker

---
+++ cyber_app/web/flutter_bootstrap.js
@@ -0,0 +1,6 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+// This app depends on live Firebase/API data. Do not register Flutter's
+// offline service worker, because it can keep an older UI after deployment.
+_flutter.loader.load();
